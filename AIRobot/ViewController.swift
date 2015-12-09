91259258e8750dd73c242b3be0bfeccbd817e701
linyc <linyc57@163.com>
2015-12-09 22:47:56 +0800

    a

--- AIRobot/ViewController.swift
+++ AIRobot/ViewController.swift
@@ -12,7 +12,7 @@ let messageFontSize:CGFloat = 17
 let toolBarMinHeight:CGFloat = 44
 
 
-class ViewController: UIViewController ,UITextViewDelegate{
+class ViewController: UITableViewController ,UITextViewDelegate{
 
 var toolBar:UIToolbar!
 var textView:UITextView!
@@ -21,9 +21,9 @@ var sendButton:UIButton!
     override var inputAccessoryView:UIView!{
         get{
             if toolBar == nil{
-                toolBar = UIToolbar(frame: CGRectMake(0,0,0,toolBarMinHeight-0.5))
+                toolBar = UIToolbar(frame: CGRectMake(0,0,UIScreen.mainScreen().bounds.size.width,toolBarMinHeight-0.5))
                 
-                textView = InputTextView(frame:CGRectZero)
+                textView = InputTextView(frame:CGRectMake(5, 5, 200, 30))
                 textView.backgroundColor = UIColor(white: 250/255, alpha: 1)
                 textView.delegate = self
                 textView.font = UIFont.systemFontOfSize(messageFontSize)
@@ -36,7 +36,7 @@ var sendButton:UIButton!
                 
                 sendButton = UIButton(type: .System)
                 sendButton.enabled = false
-                sendButton.titleLabel?.font = UIFont.boldSystemFontOfSize(17)
+                sendButton.titleLabel?.font = UIFont.boldSystemFontOfSize(15)
                 sendButton.setTitle("发送", forState: .Normal)
                 sendButton.setTitleColor(UIColor(red: 142/255, green: 142/255, blue: 147/255, alpha: 1), forState: .Disabled)
                 sendButton.setTitleColor(UIColor(red: 0.05, green: 0.47, blue: 0.91, alpha: 1.0), forState: .Normal)
@@ -44,15 +44,17 @@ var sendButton:UIButton!
                 sendButton.addTarget(self, action: "sendAction", forControlEvents: .TouchUpInside)
                 toolBar.addSubview(sendButton)
                 
-//                textView.setTranslatesAutoresizingMaskIntoConstraints(false)
-//                sendButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+                textView.translatesAutoresizingMaskIntoConstraints = false
+                sendButton.translatesAutoresizingMaskIntoConstraints = false
                 
-                toolBar.addConstraint(NSLayoutConstraint(item: textView, attribute: .Leading, relatedBy: .Equal, toItem: toolBar, attribute: .Leading, multiplier: 1, constant: 8))
-                toolBar.addConstraint(NSLayoutConstraint(item: textView, attribute: .Top, relatedBy: .Equal, toItem: toolBar, attribute: .Top, multiplier: 1, constant: 7.5))
-                toolBar.addConstraint(NSLayoutConstraint(item: textView, attribute: .Trailing, relatedBy: .Equal, toItem: sendButton, attribute: .Leading, multiplier: 1, constant: -2))
-                toolBar.addConstraint(NSLayoutConstraint(item: textView, attribute: .Bottom, relatedBy: .Equal, toItem: toolBar, attribute: .Bottom, multiplier: 1, constant: -8))
-                toolBar.addConstraint(NSLayoutConstraint(item: sendButton, attribute: .Trailing, relatedBy: .Equal, toItem: toolBar, attribute: .Trailing, multiplier: 1, constant: 0))
-                toolBar.addConstraint(NSLayoutConstraint(item: sendButton, attribute: .Bottom, relatedBy: .Equal, toItem: toolBar, attribute: .Bottom, multiplier: 1, constant: -4.5))
+                NSLayoutConstraint(item: textView, attribute: .Leading, relatedBy: .Equal, toItem: toolBar, attribute: .Leading, multiplier: 1, constant: 8).active = true
+                NSLayoutConstraint(item: textView, attribute: .Top, relatedBy: .Equal, toItem: toolBar, attribute: .Top, multiplier: 1, constant: 7.5).active = true
+                NSLayoutConstraint(item: textView, attribute: .Trailing, relatedBy: .Equal, toItem: sendButton, attribute: .Leading, multiplier: 1, constant: -2).active = true
+                NSLayoutConstraint(item: textView, attribute: .Bottom, relatedBy: .Equal, toItem: toolBar, attribute: .Bottom, multiplier: 1, constant: -8).active = true
+                
+                NSLayoutConstraint(item: sendButton, attribute: .Trailing, relatedBy: .Equal, toItem: toolBar, attribute: .Trailing, multiplier: 1, constant: 0).active = true
+                NSLayoutConstraint(item: sendButton, attribute: .Top, relatedBy: .Equal, toItem: toolBar, attribute: .Top, multiplier: 1, constant: 4.5).active = true
+                NSLayoutConstraint(item: sendButton, attribute: .Bottom, relatedBy: .Equal, toItem: toolBar, attribute: .Bottom, multiplier: 1, constant: -4.5).active = true
             }
             
             return toolBar;
@@ -65,6 +67,8 @@ var sendButton:UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.keyboardDismissMode = .Interactive
+        self.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
